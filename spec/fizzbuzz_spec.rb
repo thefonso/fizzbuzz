73eec4a96e0595df2d5c0d7e1b5020b58249f29f
thefonso <alephmage@gmail.com>
2013-12-04 10:12:34 -0600

    comments

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,17 +6,24 @@ describe 'fizzbuzz calculator' do
 		@calculator = Calculator.new
 	end
 
+	# NOTE: 
+	# this first test is solved for you (we did this one the day before)
+	# use this example to think about how to solve the next test...
+	# ...see "Instructions" below.
+
 	it 'returns 1 from 1' do
 		@calculator.calculate(1).should == 1
 	end
 
-	# instructions:
-	# solve the following tests.
-	# create a commit and push to github after EACH test is solved.
-	# follow the fail / pass pattern we discussed in class the day before
-	# use your resources (codecademy ruby glossary, ruby kickstart cheatsheet, ruby-doc.org)
-	# to figure out the code you need to place in fizzbuzz.rb 
-	# in order to make the failing test pass
+	# Instructions:
+	#
+	# Solve the following tests.
+	# Create a commit and push to github after EACH test is solved.
+	# Follow the fail / pass pattern we discussed in class the day before.
+	# Use your resources (codecademy ruby glossary, ruby kickstart cheatsheet, ruby-doc.org)
+	# to figure out the code you need to place in fizzbuzz.rb to pass each test.
+	# Note: all test MUST pass.
+	# 
 	# Share your code by making Mr. Rush (thefonso) a collaborator on your github project
 	
 
